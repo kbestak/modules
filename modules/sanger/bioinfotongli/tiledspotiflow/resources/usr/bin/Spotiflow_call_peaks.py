8285b8389f129e44379ac48bed1927f09277b0f4
Tong LI <tl10@sanger.ac.uk>
2025-02-07 12:55:30 +0000

    removed irrelevant scripts and slightly updated the script

--- modules/sanger/bioinfotongli/tiledspotiflow/resources/usr/bin/Spotiflow_call_peaks.py
+++ modules/sanger/bioinfotongli/tiledspotiflow/resources/usr/bin/Spotiflow_call_peaks.py
@@ -9,29 +9,27 @@ import fire
 from aicsimageio import AICSImage
 from spotiflow.model import Spotiflow
 import csv
-import os
-import numpy as np
 
 
-def main(image_path:str, out_dir:str, out_name:str,
-         x_min:int, x_max:int, y_min:int, y_max:int,
-         ch_ind:int=2, model_name:str="general",
-         zs:list=[0]):
+def main(
+        image_path:str, output_name:str, C:int,
+        x_min:int, x_max:int, y_min:int, y_max:int,
+        model_name:str="general",
+        T:int=0,
+        Z:int=0
+    ):
     img = AICSImage(image_path)
     lazy_one_plane = img.get_image_dask_data(
-        "ZCYX",
-        T=0, # only one time point is allowed for now
-        C=ch_ind,
-        Z=zs)
-    crop = lazy_one_plane[:, :, y_min:y_max, x_min:x_max].squeeze().compute()
+        "YX",
+        T=T,
+        C=C,
+        Z=Z
+    )
+    crop = lazy_one_plane[y_min:y_max, x_min:x_max].squeeze().compute()
     model = Spotiflow.from_pretrained(model_name)
     peaks, details  = model.predict(crop)
 
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    with open(f"{out_dir}/{out_name}", 'w', newline='') as f:
+    with open(output_name, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['y', 'x'])  # write column names
         if len(peaks) > 0:
